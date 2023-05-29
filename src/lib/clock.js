14ca8daa1b2137a35a8ed86d5a21cffb0de53a86
fchastagnol <frederic.chastagnol@pi-system.ch>
2023-05-29 10:35:53 +0200

    Add react-clock component, update next

---
+++ src/lib/clock.js
@@ -0,0 +1,20 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import Clock from 'react-clock';
+
+export default function ClientClock() {
+    const [value, setValue] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => setValue(new Date()), 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+    return (
+        <div>
+            <Clock value={value} />
+        </div>
+    );
+}
